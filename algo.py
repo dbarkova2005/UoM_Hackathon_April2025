32589e9827577dd3cdf6d7c16bd72cff58144f30
Daria Barkova <daria.barkova@student.manchester.ac.uk>
2025-04-05 13:55:14 +0100

    Retrieve sector & industry from Ticker

--- algo.py
+++ algo.py
@@ -1,3 +1,13 @@
+import yfinance as yf
+dat = yf.Ticker("MSFT")
+print(dat.info["sector"])
+print(dat.info["industry"])
+print(dat.history(period='1y'))
+
+# var
+# age & risk
+
+
 def extract_preferences(message: str):
     print("TODO")
     print(message)

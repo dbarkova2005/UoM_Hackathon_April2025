88d14c4befaf39e314ec242b0f6eae98555c5959
Daria Barkova <daria.barkova@student.manchester.ac.uk>
2025-04-05 17:07:29 +0100

    Get percentage change from tickers from two distinct start and end dates

--- algo.py
+++ algo.py
@@ -1,11 +1,46 @@
 import yfinance as yf
-dat = yf.Ticker("MSFT")
-print(dat.info["sector"])
-print(dat.info["industry"])
-print(dat.history(period='1y'))
+import pandas as pd
 
-# var
-# age & risk
+symbols = [
+    "PLTR", "MSTR", "AXON", "APP", "ARM", "LIN", "AZN", "BKR", "AVGO", "BIIB",
+    "BKNG", "CDNS", "ADBE", "CHTR", "CPRT", "CSGP", "CRWD", "CTAS", "CSCO", "CMCSA",
+    "COST", "CSX", "CTSH", "DDOG", "DXCM", "FANG", "EA", "ON", "EXC", "TTD", "FAST",
+    "GFS", "META", "FI", "FTNT", "GILD", "GOOG", "GOOGL", "HON", "INTC", "INTU", "ISRG",
+    "MRVL", "IDXX", "KDP", "KLAC", "KHC", "LRCX", "LULU", "MELI", "MAR", "MCHP", "MDLZ",
+    "MNST", "MSFT", "MU", "NFLX", "GRAL", "NVDA", "NXPI", "ODFL", "ORLY", "PCAR", "PANW",
+    "PAYX", "PDD", "PYPL", "PEP", "QCOM", "REGN", "ROST", "SBUX", "SNPS", "TSLA", "TXN",
+    "TMUS", "VRSK", "VRTX", "WBD", "WDAY", "XEL", "ZS", "ADP", "ABNB", "AMD", "CEG", "AMZN",
+    "AMGN", "AEP", "CDW", "CCEP", "ADI", "MDB", "DASH", "ROP", "ANSS", "AAPL", "AMAT", "GEHC",
+    "ASML", "TEAM", "ADSK"
+]
+
+def test_yfinance():
+    temp = ""
+    # print(dat.info["sector"])
+    # print(dat.info["industry"])
+    # print(dat.history(period='1y'))
+
+    for ticker in symbols:
+        temp = yf.Ticker(ticker)
+
+
+def analysis(start_date, end_date, avoid):
+    data = yf.download(symbols, start="2023-04-14", end="2024-12-14")
+
+    result = {}
+
+    for ticker in symbols:
+        try:
+            open_price = data['Open'][ticker].iloc[0]
+            close_price = data['Close'][ticker].iloc[-1]
+            result[ticker] = {
+                'Change %': ((close_price - open_price) / open_price) * 100
+            }
+        except Exception as e:
+            result[ticker] = f"Error: {e}"
+
+    print(result)
+    return result
 
 
 import re
@@ -44,3 +79,7 @@ with open("examples.txt", "r") as f:
     for ctx in contexts:
         msg = eval(ctx)["message"]
         print(extract_preferences(msg))
+        temp_dict = extract_preferences(msg)
+        # analysis(temp_dict.get("start_date"), temp_dict.get("end_date"), avoid = [])
+    
+analysis(start_date="", end_date="", avoid="")
